d39d01f31e0b37767a066bf5166e94e9bfa2a2e7
Cecilia <ceciliacheng10@gmail.com>
2025-10-02 02:31:17 -0700

    added ingredient scriptatble objects

---
+++ 2d-pov/Assets/Scripts/Managers/KitchenManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class KitchenManager : MonoBehaviour
+{
+    // The array to hold your dish prefabs. 
+    public GameObject[] dishPrefabs; 
+    public Transform orderWindow;
+
+    private int currentDishId;
+
+    void Awake()
+    {
+        // retrieve currentDishId
+        currentDishId = GameData.currentDishId;
+
+        // reset currentDishId
+        GameData.currentDishId = -1;
+
+        if (currentDishId >= 0 && currentDishId < dishPrefabs.Length)
+        {
+            DisplayDishPrefab(currentDishId);
+        }
+        else
+        {
+            Debug.LogError("Invalid Dish ID: " + currentDishId + 
+                           ". Make sure the ID is within the range of the dishPrefabs array size.");
+        }
+    }
+
+    private void DisplayDishPrefab(int dishID)
+    {
+        // Use the dishID as the index to get the correct prefab
+        GameObject dishPrefab = dishPrefabs[dishID];
+
+        // Instantiate (create) the prefab in the scene
+        Instantiate(dishPrefab, orderWindow.position, Quaternion.identity);  //todo: specify location in order window
+
+        Debug.Log("Successfully loaded dish: " + dishPrefab.name + " (ID: " + dishID + ")");
+    }
+}
