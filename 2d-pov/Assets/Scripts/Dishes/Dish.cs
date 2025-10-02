86d4737dce1583023ece01183e5872db1d21e8ea
Cecilia <ceciliacheng10@gmail.com>
2025-10-02 11:51:11 -0700

    added dish scriptable objects

--- 2d-pov/Assets/Scripts/Dishes/Dish.cs
+++ 2d-pov/Assets/Scripts/Dishes/Dish.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
+[ExecuteInEditMode] // run in edit mode to update visuals
+[RequireComponent(typeof(SpriteRenderer))] // ensure there's a SpriteRenderer
 public class Dish : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Recipe recipe;
+    void OnValidate()
     {
-        
+        ApplyData();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ApplyData()
     {
-        
+        if (recipe != null && recipe.icon != null)
+        {
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                sr.sprite = recipe.icon;
+                gameObject.name = recipe.dishName;
+            }
+        }
     }
+
 }
