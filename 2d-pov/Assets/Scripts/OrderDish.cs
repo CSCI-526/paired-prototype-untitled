096db2559e7cfd7c0e429f3a1352845d7541a095
DaveStutler <hxnguyen@ucdavis.edu>
2025-10-03 12:35:05 -0700

    dummy changes

--- 2d-pov/Assets/Scripts/OrderDish.cs
+++ 2d-pov/Assets/Scripts/OrderDish.cs
@@ -5,7 +5,7 @@ public class OrderDish : MonoBehaviour
     [Header("Order Details")]
     public string orderName; // Name of the dish ordered
     public string orderID; // ID of the dish ordered
-    void Start()
+    void Start() 
     {
         
 
